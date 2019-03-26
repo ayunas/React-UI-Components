825632d6d4c994ff3409d7ec72e3a5f3cef7e5f1
ayunas <ayunas@gmail.com>
2019-03-26 17:16:12 -0500

    created the Clear component and rendered it to the app

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -2,18 +2,14 @@ import React from 'react';
 import './App.css';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay.js';
 import Number from './components/ButtonComponents/Number.js';
+import Clear from './components/ButtonComponents/Clear.js';
 
 const App = () => {
   return (
     <div>
       <CalculatorDisplay />
+      <Clear />
       <Number />
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
     </div>
   );
 };
---
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/Clear.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Button.css';
+
+const Clear = () => {
+    
+    return (
+        <div>clear</div>
+    );
+    
+    
+}
+
+export default Clear;

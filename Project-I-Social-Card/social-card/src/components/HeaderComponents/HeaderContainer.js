da1a4e4327863e556085c09ec8399ca6ab132df4
ayunas <ayunas@gmail.com>
2019-03-25 22:28:49 -0500

    set up project and imported the header container into the app.js file.  imported the headertitle and placed it in the headercontainer

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from './HeaderTitle.js';
+//import Image from '.ImageThumbnail.js';
+//import
+
+const HeaderContainer = () => {
+    
+    return (
+        <HeaderTitle />
+    );
+} 
+
+
+export default HeaderContainer;
+
+
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderTitle = () => <h2>Lambda School</h2>;
+
+
+export default HeaderTitle;

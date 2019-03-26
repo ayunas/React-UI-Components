7df620461dca6c61fdd13436c7a387c18ba6221c
ayunas <ayunas@gmail.com>
2019-03-26 17:06:56 -0500

    commiting footer of social card to github

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import './Footer.css';
+import SpeechBubble from './speech-bubble.png'
 
 const Footer = () => {
     
     return (
         <footer>
             <div class='image'>
-                <img class='chat' src={require('./speech-bubble.png')}/>
+                <img class='chat' src={SpeechBubble}/>
                 <img class='retweet' src={require('./retweet.png')}/>
                 <img class='like' src={require('./like.png')}/>
                 <img class='message' src={require('./envelope.png')}/>
             </div>
         </footer>
-    )
-    
+    )   
 }
-
 
 export default Footer;
 

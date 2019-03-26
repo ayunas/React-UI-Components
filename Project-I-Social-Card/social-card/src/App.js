58bcb553ad35a0bf85021261e45ce0b82f9c9cca
ayunas <ayunas@gmail.com>
2019-03-26 09:26:56 -0500

    structured and rendered the footer in the browser

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer.js';
 import CardContainer from './components/CardComponents/CardContainer.js';
+import Footer from './components/FooterComponents/Footer.js';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <div>
             <CardContainer />
         </div>
+        <Footer />
     </div>
   );
 };
---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    
+    return (
+        <footer>
+            <img class='chat' src='#'/>
+            <img class='retweet' src='#'/>
+            <img class='like' src='#'/>
+            <img class='message' src='#'/>
+        </footer>
+    )
+    
+}
+
+
+export default Footer;
+
